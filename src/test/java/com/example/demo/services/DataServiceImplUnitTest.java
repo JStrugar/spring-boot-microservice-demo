705b831e10496455de7dcd0e270036a07495d2fa
Jovan Strugar <jovan.strugar@zuehlke.com>
2020-09-28 14:15:49 +0200

    Added unit tests for service.

--- src/test/java/com/example/demo/services/DataServiceImplUnitTest.java
+++ src/test/java/com/example/demo/services/DataServiceImplUnitTest.java
@@ -3,9 +3,11 @@ package com.example.demo.services;
 import com.example.demo.DemoApplication;
 import com.example.demo.models.DataPojo;
 import com.example.demo.repositories.DataRepository;
+import net.bytebuddy.asm.Advice;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.ArgumentCaptor;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.Mockito;
@@ -31,8 +33,32 @@ public class DataServiceImplUnitTest {
         String left = "TGV2byE=";
         DataPojo expected = new DataPojo(1, Base64.getDecoder().decode(left), null);
         dataService.saveDataLeft(id, left);
-        Mockito.verify(dataRepository, Mockito.atLeastOnce()).findById(1);
-        Mockito.verify(dataRepository, Mockito.atLeastOnce()).save(Mockito.any(DataPojo.class));
+        final ArgumentCaptor<Integer> idCaptor = ArgumentCaptor.forClass(Integer.class);
+        final ArgumentCaptor<DataPojo> dataCaptor = ArgumentCaptor.forClass(DataPojo.class);
+        Mockito.verify(dataRepository, Mockito.atLeastOnce()).findById(idCaptor.capture());
+        Mockito.verify(dataRepository, Mockito.atLeastOnce()).save(dataCaptor.capture());
+
+        Assertions.assertEquals(1, idCaptor.getValue());
+        Assertions.assertEquals(1, dataCaptor.getValue().getId());
+        Assertions.assertArrayEquals(Base64.getDecoder().decode("TGV2byE="), dataCaptor.getValue().getLeftData());
+        Assertions.assertNull(dataCaptor.getValue().getRightData());
     }
 
+
+    @Test
+    public void AddRightData() {
+        int id = 1;
+        String right = "RGVzbm8h";
+        DataPojo expected = new DataPojo(1, Base64.getDecoder().decode(right), null);
+        dataService.saveDataRight(id, right);
+        final ArgumentCaptor<Integer> idCaptor = ArgumentCaptor.forClass(Integer.class);
+        final ArgumentCaptor<DataPojo> dataCaptor = ArgumentCaptor.forClass(DataPojo.class);
+        Mockito.verify(dataRepository, Mockito.atLeastOnce()).findById(idCaptor.capture());
+        Mockito.verify(dataRepository, Mockito.atLeastOnce()).save(dataCaptor.capture());
+
+        Assertions.assertEquals(1, idCaptor.getValue());
+        Assertions.assertEquals(1, dataCaptor.getValue().getId());
+        Assertions.assertArrayEquals(Base64.getDecoder().decode("RGVzbm8h"), dataCaptor.getValue().getRightData());
+        Assertions.assertNull(dataCaptor.getValue().getLeftData());
+    }
 }
